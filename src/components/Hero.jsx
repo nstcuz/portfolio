51cc46d1198ba8d040fb2b8382bd35d022df9e0e
nstcuz <126234425+nstcuz@users.noreply.github.com>
2024-07-12 00:00:38 -0700

    safari desktop fix - hero arrow button center

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,17 +14,19 @@ const Hero = () => {
           <a href='#project-anchor' className="pop-up btn btn-primary mt-10 text-neutral-content border-neutral-content">see my works</a>
         </div>
       </div>
-      <a 
-        href="#project-anchor"
-        className='absolute mx-auto bottom-8'>
-        <svg
-          className='size-8 animate-bounce fill-neutral-content'
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          id='anchor'>
-          <path d="M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z" />
-        </svg>
-      </a>
+      <div className="flex justify-center items-center">
+        <a 
+          href="#project-anchor"
+          className='absolute mx-auto bottom-8'>
+          <svg
+            className='size-8 animate-bounce fill-neutral-content'
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            id='anchor'>
+            <path d="M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z" />
+          </svg>
+        </a>
+      </div>
       <span id=""></span>
     </section>
   );
