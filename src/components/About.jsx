891a058c8fc5e349877c3294f2afdb3574440323
nstcuz <126234425+nstcuz@users.noreply.github.com>
2024-07-12 13:01:39 -0700

    changed color on links in about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,12 +5,12 @@ import Music from './Music';
 
 const About = () => {
   const activity = [
-    'making <a href="https://www.instagram.com/p/CfEemv4ojqj/?hl=en&img_index=1"><span class="font-bold text-blue-400 letter-spread hover:border-transparent hover:border-b-neutral hover:border-dashed hover:border-b-2">focaccia</span></a> a bit too often', 
-    'currently riding a <a href="https://faworldentertainment.com/en-ca/collections/hockey"><span class="font-bold text-blue-400 letter-spread hover:border-transparent hover:border-b-neutral hover:border-dashed hover:border-b-2">hockey</span></a> skateboard',
-    'continuing my mediocre <a href="https://vanman.shop/collections/vanman-collection"><span class="font-bold text-blue-400 letter-spread hover:border-transparent hover:border-b-neutral hover:border-dashed hover:border-b-2">skincare</span> </a> routine',
-    'obviously enjoying the occasional <a href="https://yellowdogbeer.com/beers/chase-my-tail/"><span class="font-bold text-blue-400 letter-spread hover:border-transparent hover:border-b-neutral hover:border-dashed hover:border-b-2">pint</span></a>',
+    'making <a href="https://www.instagram.com/p/CfEemv4ojqj/?hl=en&img_index=1"><span class="font-bold text-[#70acc6] letter-spread hover:border-transparent hover:border-b-neutral hover:border-dashed hover:border-b-2">focaccia</span></a> a bit too often', 
+    'currently riding a <a href="https://faworldentertainment.com/en-ca/collections/hockey"><span class="font-bold text-[#70acc6] letter-spread hover:border-transparent hover:border-b-neutral hover:border-dashed hover:border-b-2">hockey</span></a> skateboard',
+    'continuing my mediocre <a href="https://vanman.shop/collections/vanman-collection"><span class="font-bold text-[#70acc6] letter-spread hover:border-transparent hover:border-b-neutral hover:border-dashed hover:border-b-2">skincare</span> </a> routine',
+    'obviously enjoying the occasional <a href="https://yellowdogbeer.com/beers/chase-my-tail/"><span class="font-bold text-[#70acc6] letter-spread hover:border-transparent hover:border-b-neutral hover:border-dashed hover:border-b-2">pint</span></a>',
     'listening to more country music than I\'m proud to admit',
-    'building <a href="#project-anchor"><span class="font-bold text-blue-400 letter-spread hover:border-transparent hover:border-b-neutral hover:border-dashed hover:border-b-2">websites</span></a>, I even like designing them'
+    'building <a href="#project-anchor"><span class="font-bold text-[#70acc6] letter-spread hover:border-transparent hover:border-b-neutral hover:border-dashed hover:border-b-2">websites</span></a>, I even like designing them'
   ];
 
 //  you can pull the playlist ID out of any playlists URL 
@@ -28,7 +28,7 @@ const About = () => {
           {activity.map((item, index) => (
             <li
               key={index}
-              className={`w-sptWidthForMargin py-2 px-2 border-t-2 border-neutral border-dashed text-center mx-auto ${index === activity.length - 1 ? 'border-b-2' : ''}`}
+              className={`w-sptWidthForMargin py-2 px-2 border-t-2 border-[#70acc6] border-dashed text-center mx-auto ${index === activity.length - 1 ? 'border-b-2' : ''}`}
               dangerouslySetInnerHTML={{ __html: item }}
             ></li>
           ))}
